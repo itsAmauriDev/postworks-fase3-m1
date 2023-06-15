89b3c1a28cbeaf2dee10a8f4eaa4de2c19601bdb
shumaker88 <shumaker225@gmail.com>
2023-06-14 21:25:41 -0600

    Se agrega postwork 5

---
+++ p5/target/generated-sources/annotations/org/bedu/java/backend/crm/controllers/mappers/ClienteMapperImpl.java
@@ -0,0 +1,11 @@
+package org.bedu.java.backend.crm.controllers.mappers;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-14T21:21:34-0600",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 17.0.7 (Amazon.com Inc.)"
+)
+public class ClienteMapperImpl implements ClienteMapper {
+}
